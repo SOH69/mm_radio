3576a80db08a9c9fb70eeef356e53e186d2d6f37
Master Mind <70486873+SOH69@users.noreply.github.com>
2024-02-06 02:43:34 +0530

    introduced new inbuilt notify

--- ui/src/components/header.svelte
+++ ui/src/components/header.svelte
@@ -1,12 +1,34 @@
 <script lang="ts">
     import { RADIODATA } from '@store/stores'
     import '@fortawesome/fontawesome-free/css/all.min.css'
+	import { ReceiveNUI } from '@utils/ReceiveNUI'
+    import { slide } from 'svelte/transition';
+    let showmsg: string = null
 
+    ReceiveNUI('notify', (msg: string) => {
+        showmsg = msg
+        setTimeout(() => {
+            showmsg = null
+        }, 5000)
+    })
 
+    function fadeSlide(node, options) {
+		const slideTrans = slide(node, options)
+		return {
+			duration: options.duration,
+            ...slideTrans
+		};
+	}
 </script>
 
-<div class="w-full h-[3vh] bg-[#18162F] flex items-center justify-between px-4 text-white text-[1.5vh]">
-    <i class="fa-solid fa-satellite-dish animate-pulse animate-infinite animate-duration-100"></i>
-    <span class="font-bold">{$RADIODATA.time}</span>
-    <i class="fa-solid fa-battery-full"></i>
-</div>
+{#if showmsg}
+    <div class="w-full h-[3vh] bg-[#18162F] flex items-center justify-between px-4 text-white text-[1.5vh] rounded-t-md">
+        <span transition:fadeSlide="{{duration: 300}}" class="font-bold overflow-hidden w-full text-center">{showmsg}</span>
+    </div>
+{:else}
+    <div transition:fadeSlide="{{duration: 300}}" class="w-full h-[3vh] bg-[#18162F] flex items-center justify-between px-4 text-white text-[1.5vh] rounded-t-lg">
+        <i class="fa-solid fa-satellite-dish animate-pulse animate-infinite animate-duration-100"></i>
+        <span class="font-bold">{$RADIODATA.time}</span>
+        <i class="fa-solid fa-battery-full"></i>
+    </div>
+{/if}
