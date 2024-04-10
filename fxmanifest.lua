03c86f89c27f9bf3de0dc283eba92fba5b65379b
Master Mind <70486873+SOH69@users.noreply.github.com>
2024-04-10 13:41:46 +0530

    version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game "gta5"
 
 author "Master Mind"
-version '2.0.2'
+version '2.0.3'
 description 'A beautiful Radio Resource for FiveM'
 repository 'https://github.com/SOH69/mm_radio'
 
