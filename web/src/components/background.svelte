5dc500ad8c69fd4d8880fd065ad1d0af3bc81379
Master Mind <70486873+SOH69@users.noreply.github.com>
2024-04-15 02:34:17 +0530

    add slide up animation on radio open

--- web/src/components/background.svelte
+++ web/src/components/background.svelte
@@ -1,31 +1,21 @@
 <script lang='ts'>
 	import Radio from '@components/radio.svelte';
     import { tweened } from 'svelte/motion';
-    import { VISIBLE, SHOW, RADIODATA, MOVERADIO } from '@stores/stores';
+    import { SHOW, RADIODATA, MOVERADIO } from '@stores/stores';
     import { SendEvent } from '@utils/eventsHandlers';
     import { Send } from '@enums/events';
     import radioBgImg from '@assets/overlay.png'
 
-    let visible: boolean = false;
-
-    const bottom = tweened(visible? $RADIODATA.userData.radio.coords.y:-61, {
-		duration: 300,
-	});
+    const BOTTOM = tweened(-1000);
 
     SHOW.subscribe((visible: boolean) => {
-		bottom.set(visible? $RADIODATA.userData.radio.coords.y:-61);
+		BOTTOM.set(visible? $RADIODATA.userData.radio.coords.y:-1000);
 	});
-
-    bottom.subscribe((value: number) => {
-        if (value === -61 && visible) {
-            VISIBLE.set(false);
-        }
-    });
 
     function dragMe(node: any) {
         let moving = false;
         let right = parseInt((node.style.right).replace('px', ''));
-        let bottom = parseInt((node.style.bottom).replace('px', ''));
+        let bottom = $RADIODATA.userData.radio.coords.y;
         
         node.style.position = 'absolute';
         node.style.bottom = `${bottom}px`;
@@ -41,7 +31,7 @@
             if (moving) {
                 right -= e.movementX;
                 bottom -= e.movementY;
-
+                
                 right = Math.min(Math.max(right, 0), window.innerWidth - node.offsetWidth);
                 bottom = Math.min(Math.max(bottom, 0), window.innerHeight - node.offsetHeight);
 
@@ -62,7 +52,7 @@
 </script>
 
 <div class="w-[326px] h-[660px] absolute px-10 select-none" 
-    style="transform: scale({($RADIODATA.userData.radioSizeMultiplier*2)/100}); bottom: {$SHOW?$RADIODATA.userData.radio.coords.y:-9999}px; right: {$RADIODATA.userData.radio.coords.x}px; cursor:{$MOVERADIO? 'move':'pointer'};" 
+    style="transform: scale({($RADIODATA.userData.radioSizeMultiplier*2)/100}); bottom: {$BOTTOM}px; right: {$RADIODATA.userData.radio.coords.x}px; cursor:{$MOVERADIO? 'move':'pointer'};" 
     use:dragMe
 >
     <img alt='bg' src={radioBgImg} class="z-[8] relative w-full h-full" draggable="false">
