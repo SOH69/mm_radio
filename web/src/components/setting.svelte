564c73b7cc115d6ec0f3abc99c4b538f60d7583e
Master Mind <70486873+SOH69@users.noreply.github.com>
2024-04-22 22:06:00 +0530

    added settings page locales

--- web/src/components/setting.svelte
+++ web/src/components/setting.svelte
@@ -36,13 +36,21 @@
     function toggleRadioMove(event: any) {
         MOVERADIO.set(event.target.checked);
     }
+
+    function toggleMovement(event: any) {
+        RADIODATA.update((data: any) => {
+            data.userData.allowMovement = event.target.checked;
+            return data;
+        })
+        SendEvent(Send.allowMovement, event.target.checked);
+    }
     
 </script>
 
-<div class="w-full h-[254px] drop-shadow-md p-4">
+<div class="w-full h-[223.5px] drop-shadow-md p-4">
     <div class="w-full h-full bg-[#18162F] rounded-[6px] text-white font-bold overflow-scroll no-scrollbar px-4 py-2">
         <div class="flex flex-col w-full gap-2 border-b-[1px] border-gray-700 pb-4">
-            <span class="text-[14px]">Radio Settings</span>
+            <span class="text-[14px]">{$RADIODATA.locale['ui.radio_settings']}</span>
             <div class="flex gap-2">
                 <div class="h-[20px] w-[18px] rounded-md flex justify-center items-center bg-[#0e0d1a]">
                     <i class="fa-solid fa-up-right-and-down-left-from-center text-[8px]"></i>
@@ -55,7 +63,7 @@
                 <div class="h-[20px] w-[18px] rounded-md flex justify-center items-center bg-[#0e0d1a] gap-2">
                     <i class="fa-solid fa-maximize text-[8px]"></i>
                 </div>
-                <span class="text-[10px] flex justify-center items-center">Move Radio</span>
+                <span class="text-[10px] flex justify-center items-center">{$RADIODATA.locale['ui.move_radio']}</span>
                 <div class="h-[20px] flex items-center pl-6">
                     <label class="inline-flex items-center cursor-pointer">
                         <input type="checkbox" value="" class="sr-only peer" checked={$MOVERADIO} on:input={toggleRadioMove}>
@@ -63,10 +71,22 @@
                     </label>
                 </div>
             </div>
+            <div class="flex gap-2">
+                <div class="h-[20px] w-[18px] rounded-md flex justify-center items-center bg-[#0e0d1a] gap-2">
+                    <i class="fa-solid fa-maximize text-[8px]"></i>
+                </div>
+                <span class="text-[10px] flex justify-center items-center">{$RADIODATA.locale['ui.allow_move']}</span>
+                <div class="h-[20px] flex items-center pl-6">
+                    <label class="inline-flex items-center cursor-pointer">
+                        <input type="checkbox" value="" class="sr-only peer" checked={$RADIODATA.userData.allowMovement} on:input={toggleMovement}>
+                        <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                    </label>
+                </div>
+            </div>
         </div>
 
-        <div class="flex flex-col w-full gap-4 border-b-[1px] border-gray-700 pb-4 pt-4">
-            <span class="text-[14px]">Overlay Settings</span>
+        <div class="flex flex-col w-full gap-4 pb-4 pt-4">
+            <span class="text-[14px]">{$RADIODATA.locale['ui.overlay_settings']}</span>
             <div class="flex gap-2">
                 <div class="h-[20px] w-[18px] rounded-md flex justify-center items-center bg-[#0e0d1a]">
                     <i class="fa-solid fa-user text-[8px]"></i>
