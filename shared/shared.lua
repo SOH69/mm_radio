f5d13659071559004aadd821ba1a256e9c480b12
Master Mind <70486873+SOH69@users.noreply.github.com>
2024-06-01 22:07:39 +0530

    added type to variables

--- shared/shared.lua
+++ shared/shared.lua
@@ -1,5 +1,14 @@
+---@type number
 Shared.MaxFrequency = 500.00 -- Max Limit of Radio Channel
 
+---@class Jammer
+---@field state boolean
+---@field model string
+---@field permission string[]
+---@field default table
+---@field range table
+
+---@type Jammer
 Shared.Jammer = {
     state = true, -- to use jammer system or not 
     model = 'm23_2_prop_m32_jammer_01a', -- prop to spawn for jammer
@@ -13,17 +22,24 @@ Shared.Jammer = {
     }
 }
 
+---@type string[]
 Shared.RadioItem = {
-    'radio',
-    'wrench'
+    'radio'
 }
 
+---@class Battery
+---@field state boolean
+---@field consume number
+---@field depletionTime number
+
+---@type Battery
 Shared.Battery = {
     state = true, -- to use battery system or not
     consume = 1, -- battery consume rate
     depletionTime = 1, -- in minute, every 1 minute battery will decrease by consume value
 }
 
+---@type [string]: string
 Shared.RadioNames = {
     ["1"] = "MRPD CH#1", -- channel value 1
     ["1.%"] = "MRPD CH#1", -- channel value 1.%%%% string formatter
