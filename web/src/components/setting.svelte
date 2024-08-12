efcc3ba3cafdcecbae70703005fd78c22e3bbaa2
Master Mind <70486873+SOH69@users.noreply.github.com>
2024-08-12 23:36:05 +0530

    added new radio use sound toggle

--- web/src/components/setting.svelte
+++ web/src/components/setting.svelte
@@ -8,7 +8,7 @@
     let radioSize = $RADIODATA.userData.radioSizeMultiplier;
 
     function saveData() {
-        SendEvent(Send.saveData, inputValue)
+        SendEvent(Send.saveData, {name: inputValue, radioId: $RADIODATA.radioId})
         changeTab('members')
     }
 
@@ -29,7 +29,6 @@
     }
 
     function handleSliderRelease() {
-        if (!$RADIODATA.onRadio) return;
         SendEvent(Send.updateRadioSize, { overlay: overlaySize, radio: radioSize })
     }
 
@@ -55,8 +54,8 @@
     
 </script>
 
-<div class="w-full h-[223.5px] drop-shadow-md p-4">
-    <div class="w-full h-full bg-[#18162F] rounded-[6px] text-white font-bold overflow-scroll no-scrollbar px-4 py-2">
+<div class="w-full h-[223.5px] p-4">
+    <div class="w-full h-full bg-[#18162F] rounded-[6px] text-white font-bold overflow-scroll no-scrollbar px-4 py-2 shadow-md shadow-[#18162F]/40">
         <div class="flex flex-col w-full gap-2 border-b-[1px] border-gray-700 pb-4">
             <span class="text-[14px]">{$RADIODATA.locale['ui.radio_settings']}</span>
             <div class="flex gap-2">
@@ -105,26 +104,28 @@
             </div>
         </div>
 
-        <div class="flex flex-col w-full gap-4 pb-4 pt-4">
-            <span class="text-[14px]">{$RADIODATA.locale['ui.overlay_settings']}</span>
-            <div class="flex gap-2">
-                <div class="h-[20px] w-[18px] rounded-md flex justify-center items-center bg-[#0e0d1a]">
-                    <i class="fa-solid fa-user text-[8px]"></i>
+        {#if $RADIODATA.overlay != 'never'}
+            <div class="flex flex-col w-full gap-4 pb-4 pt-4">
+                <span class="text-[14px]">{$RADIODATA.locale['ui.overlay_settings']}</span>
+                <div class="flex gap-2">
+                    <div class="h-[20px] w-[18px] rounded-md flex justify-center items-center bg-[#0e0d1a]">
+                        <i class="fa-solid fa-user text-[8px]"></i>
+                    </div>
+                    <input type="text" class="h-[20px] w-[106px] bg-[#212146] text-white px-4 rounded-md text-[10px]" bind:value={inputValue} />
                 </div>
-                <input type="text" class="h-[20px] w-[106px] bg-[#212146] text-white px-4 rounded-md text-[10px]" bind:value={inputValue} />
+                <div class="flex gap-2">
+                    <div class="h-[20px] w-[18px] rounded-md flex justify-center items-center bg-[#0e0d1a]">
+                        <i class="fa-solid fa-up-right-and-down-left-from-center text-[8px]"></i>
+                    </div>
+                    <div class="h-[20px] w-[106px] flex justify-center items-center">
+                        <input type="range" bind:value={overlaySize} class="h-2 w-[106px] bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" on:input={changeOverlaySize} on:mouseup={handleSliderRelease} disabled={$MOVERADIO} min="25" max="75"/>
+                    </div>
+                </div>
             </div>
-            <div class="flex gap-2">
-                <div class="h-[20px] w-[18px] rounded-md flex justify-center items-center bg-[#0e0d1a]">
-                    <i class="fa-solid fa-up-right-and-down-left-from-center text-[8px]"></i>
-                </div>
-                <div class="h-[20px] w-[106px] flex justify-center items-center">
-                    <input type="range" bind:value={overlaySize} class="h-2 w-[106px] bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" on:input={changeOverlaySize} on:mouseup={handleSliderRelease} disabled={$MOVERADIO} min="25" max="75"/>
-                </div>
-            </div>
-        </div>
+        {/if}
     </div>
 </div>
 
 <div class="w-full h-[4] px-4 flex gap-2 pb-4">
-    <button class="w-full py-3 bg-[#18162F] flex justify-center items-center gap-[11px] text-white text-[11px] rounded-[6px] font-bold drop-shadow-md" on:click={() => saveData()}>{$RADIODATA.locale['ui.save']}</button>
+    <button class="w-full py-3 bg-[#18162F] flex justify-center items-center gap-[11px] text-white text-[11px] rounded-[6px] font-bold shadow-md shadow-[#18162F]/40" on:click={() => saveData()}>{$RADIODATA.locale['ui.save']}</button>
 </div>
